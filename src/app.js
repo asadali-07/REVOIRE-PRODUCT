d9481dfee7ef79b86a322d8c08d644803c452c45
Asad Ali <mo.asadali007@gmail.com>
2025-11-08 18:16:25 +0530

    Product on github

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
   credentials: true 
 }));
